941d5d71629ecce2fad62ad59c7ec6c553a1c9a8
Nick Smit <nick.smit@gmail.com>
2015-05-27 17:43:03 -0400

    added the brain regions and scales column to the mongodb and csv

--- read_neurp.py
+++ read_neurp.py
@@ -5,17 +5,21 @@ def read_data(fname):
     my_file = open(fname, "r")
     lines = my_file.read().split("\n\n\n")
     my_file.close()
+    bterms = set(format_brain("mesh_brain3.txt"))
+    sterms = set(format_scale("mmy_index.txt"))
     formatted = [[l.replace("\n", " ") for l in line.split("\n\n")[1::]] for line in lines]
     form2 = [list(operator.itemgetter(0, 3, -1)(line)) for line in formatted if len(line) > 3]
-    with open("neuro_words.csv", "wb") as neuro_words:
+    with open("neuro_words2.csv", "wb") as neuro_words:
         writer = csv.writer(neuro_words)
-        header = ['title', 'content', 'pmid']
+        header = ['title', 'content', 'pmid', 'scales', 'brain regions']
         writer.writerow(header)
         for row in form2:
+            r = set(row[1].split(' '))
+            scales = [" | ".join(list(r.intersection(sterms)))]
+            bregions = [" | ".join(list(r.intersection(bterms)))]
+            row + scales + bregions
             writer.writerow(row)
     return "Write complete"
-
-# print read_data("pubmed_result.txt")
 
 def format_brain(fname):
     my_file = open(fname, "r")
@@ -26,6 +30,8 @@ def format_scale(fname):
     lines = list(set(my_file.read().split("\r")))
     return [line.split(',')[0] for line in lines]
 
+print read_data("pubmed_result.txt")
 # pp(format_brain("mesh_brain3.txt"))
-pp(format_scale("mmy_index.txt"))
-pp(len(format_scale("mmy_index.txt")))
+# pp(format_scale("mmy_index.txt"))
+# pp(len(format_scale("mmy_index.txt")))
+
