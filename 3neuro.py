27dc1ea8de6998c06b5e5b412e24fef70c76c3fe
Nick Smit <nick.smit@gmail.com>
2015-05-29 10:43:36 -0400

    removed commented code and corrected empty vals bug

--- 3neuro.py
+++ 3neuro.py
@@ -1,42 +1,45 @@
-import csv, urllib2, os, operator, string, itertools, re, json
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import csv, urllib2, os, operator, string, itertools, re, json, codecs, nltk
 from pprint import pprint as pp
 from nltk.corpus import stopwords
 from collections import Counter
+from nltk.tag import pos_tag
 
 def format_brain(fname):
     my_file = open(fname, "r")
     read_file = my_file.read().lower()
     my_file.close()
-    return list(set(read_file.split("\n")))
+    return list(set([line.strip() for line in read_file.split("\n") if line]))
 
 def format_scale(fname):
-    my_file = open(fname, "r")
+    my_file = codecs.open(fname, "r")
     read_file = my_file.read().lower()
     my_file.close()
-    # lines = list(set(my_file.read().lower().split("\r")))
-    # r"(\w)(\w+)(\w)"
-    # subbed_file = [ re.sub(r'[^a-zA-Z0-9_]', ' ', line) for line in read_file.split("\r") if line]
-    subbed_file = [ re.sub(r'\W+', ' ', line) for line in read_file.split("\r") if line]
-    return list(set(subbed_file))
-    # return [line for line in lines]
+    subbed_file = list(set([ "".join([l for l in re.sub(r"\W+(?<![\-.])", " ", line)]).strip() for line in read_file.split("\r") if line]))
+    return [line for line in subbed_file if line]
 
 def read_data(fname):
     my_file = open(fname, "r")
-    read_file = my_file.read()
-    lines = read_file.split("\n\n\n")
+    lines = my_file.read().lower().split("\n\n\n")
     my_file.close()
 
-    bterms = set(format_brain("mesh_brain3.txt"))
-    # sterms = set(format_scale("mmy_index.txt"))
+    stops = stopwords.words('english') + [string.punctuation.replace("-","")]
+    bterms = set(format_brain("mesh_brain4.txt"))
     sterms = set(format_scale("psych_scales.txt"))
-    snbterms = set(format_brain("mesh_brain3.txt") + format_scale("mmy_index.txt"))
-    stop = stopwords.words('english')
-    lines2 = ["\n\n".join([l for l in line.split("\n\n")]) for line in lines]
-    formatted = [" ".join([l for l in re.sub(r'\W+', ' ', line).split() if len(l) > 2]) for line in lines2]
+
+    print "TERMS ARE LOADED UP!!"
+
+    lines2 = [re.sub(r"\W+(?<![\-.])", " ", line.decode('utf-8').split("\n\n")[4] for line in lines if len(line.decode('utf-8').split("\n\n")[4])>500 ]
+
+    print "SHIT HAS BEEN FORMATTED AND READY TO LOOP!!"
+
     my_hash = {}
-    for index, abstract in enumerate(formatted):
-      # if index == 100:
-      #   break
+    for index, abstract in enumerate(lines2):
+      print "="*150
+      print str(index)
+      print "="*150
       for sterm in sterms:
         if sterm in abstract:
           bees = [bterm for bterm in bterms if bterm in abstract]
@@ -44,30 +47,23 @@ def read_data(fname):
             for bee in bees:
               combined = sterm + " | " + bee
               if combined in my_hash:
+                print "/:"*index+"/|:v:|\:\n/|o|\." + "/:"*(index/2)
                 my_hash[combined] += 1
               else:
                 my_hash[combined] = 1
-          # if sterm in my_hash:
-          #   my_hash[sterm] = my_hash[sterm] + [bterm for bterm in bterms if bterm in abstract]
-          # else:
-          #   my_hash[sterm] = [bterm for bterm in bterms if bterm in abstract]
 
-    with open('result2.json', 'w') as fp:
+    with open('result_final.json', 'w') as fp:
         json.dump(my_hash, fp)
 
     return my_hash
-    # with open("nw.csv", "wb") as neuro_words:
-    #     writer = csv.writer(neuro_words)
-    #     header = ['title', 'content', 'pmid', 'scales', 'brain regions']
-    #     writer.writerow(header)
-    #     for index, row in enumerate(form2):
-    #         writer.writerow(row)
-    # return "Write complete"
 
 
+# scales_t = format_scale("psych_scales.txt")
+# scales_t.sort()
 
-
-pp(read_data("pubmed_result.txt"))
-# pp(format_scale("psych_scales.txt")[:100])
 # pp(read_data("pubmedsmall.txt"))
+# print read_data("pubmedsmall.txt")
 # pp(len(read_data("pubmedsmall.txt")))
+
+read_data("pubmed_result.txt")
+# pp(format_brain("mesh_brain4.txt")[:50])
